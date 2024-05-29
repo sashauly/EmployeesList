a3498d749586af972645bf95c5bed32e9e9d6b03
sashauly <sashauly.code@ya.ru>
2024-05-29 09:41:19 +0300

    feat: add redux devtools extension

--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,12 @@
+import { composeWithDevTools } from "@redux-devtools/extension";
 import { applyMiddleware, createStore } from "redux";
 import { thunk } from "redux-thunk";
 import rootReducer from "./reducers";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export type AppDispatch = typeof store.dispatch;
 
