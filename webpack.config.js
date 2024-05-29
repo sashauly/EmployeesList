391ee3c50f766a6f4ca2a2f0bc0fef6c3f797a42
sashauly <sashauly.code@ya.ru>
2024-05-29 18:55:06 +0300

    feat: update deps, so css modules can work now

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,77 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-
-const cssRegex = /\.css$/;
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.tsx",
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "build"),
+    filename: "[name].[contenthash].js",
+    publicPath: "",
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
+      inject: true,
+      template: "public/index.html",
+    }),
+    new ForkTsCheckerWebpackPlugin({
+      async: false,
+    }),
+    new ESLintPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
     }),
   ],
   module: {
-    strictExportPresence: true,
     rules: [
       {
-        test: /\.(ts|tsx)$/,
+        test: /\.(ts|js)x?$/i,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
           options: {
             presets: [
               "@babel/preset-env",
-              ["@babel/preset-react", { runtime: "automatic" }],
+              [
+                "@babel/preset-react",
+                {
+                  runtime: "automatic",
+                },
+              ],
               "@babel/preset-typescript",
             ],
-            cacheDirectory: true,
+            plugins: [
+              [
+                "@babel/plugin-transform-runtime",
+                {
+                  regenerator: true,
+                },
+              ],
+            ],
           },
         },
       },
       {
-        test: cssRegex,
-        use: [
-          "style-loader",
+        test: /\.css$/i,
+        oneOf: [
           {
-            loader: "css-loader",
-            options: {
-              esModule: true,
-              modules: {
-                namedExport: false,
-                localIdentName: "[local]__[contenthash:base64:5]",
+            test: /\.module\.css$/i,
+            use: [
+              MiniCssExtractPlugin.loader,
+              {
+                loader: "css-loader",
+                options: {
+                  modules: {
+                    localIdentName: "[name]__[local]__[contenthash:base64:5]",
+                  },
+                },
               },
-            },
+            ],
+          },
+          {
+            use: [MiniCssExtractPlugin.loader, "css-loader"],
           },
         ],
       },
@@ -52,5 +79,13 @@ module.exports = {
   },
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
+  },
+
+  devtool: "inline-source-map",
+  devServer: {
+    static: path.join(__dirname, "build"),
+    historyApiFallback: true,
+    port: 3000,
+    open: true,
   },
 };
